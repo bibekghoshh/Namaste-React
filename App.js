5ac3b9b69db66a65a85d166a211de306cc9a08f5
bibekghoshh <bibek7478@gmail.com>
2023-09-14 23:02:28 +0530

    ignited the app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello React my friend"
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is H1 tag"),
+    React.createElement("H1", {}, "This is H2 tag"),
+  ]),
+  React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"Created a h1 tag using React createelement"),
+    React.createElement("h2",{},"created this element using React")
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
