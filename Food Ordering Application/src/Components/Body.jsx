f5da15ecd5b27c5358dc0c7591e589bfc8e043f6
bibekghoshh <bibek7478@gmail.com>
2023-09-19 14:50:59 +0530

    implemented Fetch API

--- Food Ordering Application/src/Components/Body.jsx
+++ Food Ordering Application/src/Components/Body.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RestaurentCard from '../Cards/RestaurentCard';
 
-import {restaurants} from '../data/restaurants.js';
-
-// console.log(restaurants);
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants]=  useState([]);
+
+    useEffect(()=>{
+      fetchData();
+    },[]);
+
+    const fetchData=async()=>{
+      const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const Json=await data.json();
+      // console.log(Json);
+      // console.log(Json?.data.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setListOfRestaurants(Json?.data.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
   return (
     <div>
         <div className='search-container'>
             <input type="text" name="text" id="search" placeholder='Enter Your Location' />
             <button className='search-btn'>Search</button>
         </div>
+        <div>
+          <button className='top-restaurant-btn' onClick={()=>{
+            const filterRes=listOfRestaurants.filter((res)=>(res.info.avgRating>4.2));
+            setListOfRestaurants(filterRes);
+          }}>Top Rated Restaurants</button>
+        </div>
         <div className='Res-Container'>
             {/* <RestaurentCard resdata={restaurants[0]}/> */}
-            {restaurants.map((restaurant)=>(<RestaurentCard key={restaurant.info.id} resdata={restaurant}/>))}
+            {listOfRestaurants.map((restaurant)=>(<RestaurentCard key={restaurant.info.id} resdata={restaurant}/>))}
         </div>
     </div>
   )
